0a7ebabb6f42c1a0c33100e49b349a37f891412b
elid venega <venegaelid@gmail.com>
2021-09-28 20:50:00 -0700

    Work on JS to output the right answer

--- js-personal-projects/root-number/js/main.js
+++ js-personal-projects/root-number/js/main.js
@@ -13,14 +13,29 @@
 // var input = parseInt(prompt("Enter a number:"));
 // var result = Math.sqrt(input)
 
-const button = document.querySelector("#btn");
+// const button = document.querySelector("#btn");
 
-button.addEventListener("click", () => {
+// button.addEventListener("click", () => {
+//     // const rootSum = function (number) {
+//     //     return Math.sqrt(number)
+//     // }
+//     const input = document.querySelector("#text").innerHTML = "It works";
+//     document.querySelector("#output").innerHTML = input;
+
+//     // output.innerHTML = input;
+
+// })
+
+const txt = document.querySelector("#text");
+
+txt.addEventListener("click", () => {
     // const rootSum = function (number) {
     //     return Math.sqrt(number)
     // }
-    const input = document.querySelector("#text").innerHTML = Math.sqrt();
-    document.querySelector("#output").innerHTML = input;
+
+    let writing;
+    const btn = document.querySelector("#btn").innerHTML = `${writing}`;
+    document.querySelector("#output").innerHTML = btn;
 
     // output.innerHTML = input;
 
