8f650fff3b7aa7734f8d29b5c4153ce8b73ca111
elid venega <venegaelid@gmail.com>
2021-09-29 23:27:54 -0700

    Work on lat part of peojecr

--- js-personal-projects/root-number/js/main.js
+++ js-personal-projects/root-number/js/main.js
@@ -3,40 +3,23 @@
 // Goal of project is to get the root number
 // Figure out how to get root number
 
-// console.log(rootSum(25));
-
-// const answer = document.querySelector('#submit').innerHTML = rootSum();
-
-// document.querySelector('#text').innerHTML = answer;
 
 // one solution
-// var input = parseInt(prompt("Enter a number:"));
-// var result = Math.sqrt(input)
+const input = parseInt(prompt("Enter a number:"));
+const result = Math.sqrt(input)
+
+
+// Will work on this and make it work
+
 
 // const button = document.querySelector("#btn");
 
 // button.addEventListener("click", () => {
-//     // const rootSum = function (number) {
-//     //     return Math.sqrt(number)
-//     // }
-//     const input = document.querySelector("#text").innerHTML = "It works";
-//     document.querySelector("#output").innerHTML = input;
+//     const rootSum = function (number) {
+//         const input = document.querySelector("#text").innerHTML = `${number}`;
+//         return Math.sqrt(input)
+//     }
+//     //const input = document.querySelector("#text").innerHTML = "It works";
+//     document.querySelector("#output").innerHTML = rootSum();
 
-//     // output.innerHTML = input;
-
-// })
-
-const txt = document.querySelector("#text");
-
-txt.addEventListener("click", () => {
-    // const rootSum = function (number) {
-    //     return Math.sqrt(number)
-    // }
-
-    let writing;
-    const btn = document.querySelector("#btn").innerHTML = `${writing}`;
-    document.querySelector("#output").innerHTML = btn;
-
-    // output.innerHTML = input;
-
-})
+//})
