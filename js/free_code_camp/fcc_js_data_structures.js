3a3b0fa987b94592b8302a9e76e768fee4276ea5
elidvenega <venegaelid@gmail.com>
2022-07-05 17:50:24 -0700

    Worked on FCC

---
+++ js/free_code_camp/fcc_js_data_structures.js
@@ -0,0 +1,35 @@
+// Generate Random Whole Numbers within a Range
+
+// function randomRange(myMin, myMax) {
+// return Math.floor(Math.random() * (myMax - myMin + 1) + myMin);
+//   }
+
+//   console.log(randomRange(10,20));
+
+// Use Recursion to Create a Countdown
+
+// function countup(n) {
+//   if (n < 1) {
+//     return [];
+//   } else {
+//     const countArray = countup(n - 1);
+//     countArray.unshift(n);
+//     return countArray;
+//   }
+// }
+// console.log(countup(5));
+
+//Use Recursion to Create a Range of Numbers
+
+function rangeOfNumbers(startNum, endNum) {
+  if (startNum <= endNum) {
+    const countUp = rangeOfNumbers(startNum + 1);
+    countUp.push(endNum);
+    return countUp;
+  } else {
+    console.log("test fail");
+  }
+}
+
+console.log(rangeOfNumbers(5, 10));
+// output [5,6,7,8,9,10]
