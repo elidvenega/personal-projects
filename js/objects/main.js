787b9827fb864be74eaa81a32d1f4de234ef8560
elidvenega <venegaelid@gmail.com>
2022-07-21 23:00:17 -0700

    Worked with objects

---
+++ js/objects/main.js
@@ -0,0 +1,25 @@
+// const obj = {
+//   arr: ["hello", "hi", "Today"],
+//   arr2: ["great", "wolfs", "t-rex"],
+//   key: 80,
+//   // allArr: [...arr, ...arr2]
+//   // output(input1, input2) {
+//   //   return `${input1} ${input2}`;
+//   // },
+//   output() {
+//     return `${this.arr} ${this.arr2}`;
+//   },
+// };
+
+// // console.log(obj.output(obj.arr, obj.arr2));
+// // console.log(obj.output());
+// console.log(obj[key]);
+
+const food = {
+  meal: "fish and veggies",
+};
+
+let breakfast = food['meal']
+
+console.log(breakfast);
+//console.log('hello');
