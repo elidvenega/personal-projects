b53f5c5879ea1a00a0b9e97d148f0307694b12ff
elidvenega <robotsmailNinja@gmail.com>
2024-02-12 23:41:29 -0800

    Worked with python

--- python/website-practice/hello.py
+++ python/website-practice/hello.py
@@ -2,8 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+intro = "hello intro python"
+
 @app.route("/")
 def hello_world():
     return """"
     <h1>Python Learning</h1>
-    <p>Hello, World!</p>"""
+    <p>Hello, World!</p>
+     
+    """
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
