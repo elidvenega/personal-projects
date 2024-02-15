0a0763df6fc023d8c89b48bb925b7ae8577122c1
elidvenega <robotsmailNinja@gmail.com>
2024-02-15 13:04:45 -0800

    Error was fixed

--- front-end/react-projects/react-docs-learning/src/components/props/AllProps.jsx
+++ front-end/react-projects/react-docs-learning/src/components/props/AllProps.jsx
@@ -4,7 +4,7 @@ export default function AllProps() {
   return (
     <>
       <Props name="Jack" age="34" hobby="to play Guitar" exercise="Walking" />
-      <Props name="Jose" age="21" hobby="Anime" exercise="Running">
+      <Props name="Jose" age="21" hobby="Anime" exercise="Running" />
       <Props name="David" age="25" hobby="Soccer" exercise="Jumpoing Rope" />
     </>
   );
