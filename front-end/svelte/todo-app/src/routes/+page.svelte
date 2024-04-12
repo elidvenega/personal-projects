825e9b60816d56e36201e5068b9206e94d8bdf6e
elidvenega <robotsmailNinja@gmail.com>
2024-04-11 19:08:59 -0700

    Worked on Svelte

--- front-end/svelte/todo-app/src/routes/+page.svelte
+++ front-end/svelte/todo-app/src/routes/+page.svelte
@@ -1,35 +1,8 @@
 <script>
-    let newItem = '';
-	
-    let todoList = [{text: 'Write my first post', status: true},
-                    {text: 'Upload the post to the blog', status: false},
-                    {text: 'Publish the post at Facebook', status: false}];
-	
-	function addToList() {
-		todoList = [...todoList, {text: newItem, status: false}];
-		newItem = '';
-	}
-	
-	function removeFromList(index) {
-		todoList.splice(index, 1)
-		todoList = todoList;
-    }
+  // import SvelteTodoExample from "./SvelteTodoExample.svelte"
+  import TodoList from "./TodoList.svelte";
 </script>
 
-<input bind:value={newItem} type="text" placeholder="new todo item..">
-<button on:click={addToList}>Add</button>
+<!-- <SvelteTodoExample /> -->
 
-<br/>
-{#each todoList as item, index}
-	<input bind:checked={item.status} type="checkbox">
-	<span class:checked={item.status}>{item.text}</span>
-	<span on:click={() => removeFromList(index)}>❌</span>
-	<br/>
-{/each} 
-
-
-<style> 
-	.checked {
-        text-decoration: line-through;
-    }
-</style> 
+<TodoList />
---
+++ front-end/svelte/todo-app/src/routes/TodoList.svelte
@@ -0,0 +1,67 @@
+<script>
+  let newItem = "";
+
+  let todoList = [
+    {
+      task: "Go hiking",
+      status: false,
+    },
+    {
+      task: "Get groceries",
+      status: false,
+    },
+    {
+      task: "Washing",
+      status: false,
+    },
+    {
+      task: "Study",
+      status: false,
+    },
+  ];
+
+  function addTodo() {
+    todoList = [...todoList, { task: newItem, status: false}]
+    newItem = "";
+  }
+
+  function deleteTodo() {
+    
+  }
+</script>
+
+<h1>Svelte Todo List</h1>
+<div class="todo-list-container">
+  <input class="input" placeholder="add todo" type="text" name="todo-list" />
+  <button class="add-todo-button" on:click={deleteTodo}>Add Todo</button>
+</div>
+
+<style>
+  :root {
+    --color-one: #f97316;
+    --font-size: 1rem;
+  }
+
+  h1 {
+    color: var(--color-one);
+    text-align: center;
+    font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+      "Lucida Sans", Arial, sans-serif;
+    font-size: calc(var(--font-size) * 3);
+  }
+
+  .todo-list-container {
+    display: flex;
+    justify-content: center;
+    margin-top: 2rem;
+    gap: 5px;
+  }
+  /* 
+  input {
+    width: 12rem;
+  }
+
+  .add-todo-button {
+    width: 4rem;
+  } */
+</style>
