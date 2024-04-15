051e13a3d5626a5744f6be65bc37bdd43fded793
elidvenega <robotsmailNinja@gmail.com>
2024-04-15 00:17:13 -0700

    Updated

--- front-end/svelte/todo-app/src/routes/TodoList.svelte
+++ front-end/svelte/todo-app/src/routes/TodoList.svelte
@@ -21,7 +21,7 @@
   ];
 
   function addTodo() {
-    todoList = [...todoList, { task: newItem, status: false}]
+    todoList = [...todoList, { task: newItem, status: false }];
     newItem = "";
   }
 
@@ -33,26 +33,21 @@
 
 <h1>Svelte Todo List</h1>
 <div class="todo-list-container">
-  <input class="input" placeholder="add todo" type="text" name="todo-list" />
-  <button class="add-todo-button" on:click={deleteTodo}>Add Todo</button>
-  <br/>
+  <input bind:value={newItem} class="input" placeholder="add todo" type="text" name="todo-list" />
+  <button class="add-todo-button" on:click={addTodo}>Add Todo</button>
+  <br />
   {#each todoList as item, index}
-  <input bind:checked={item} type="checkbox">
-    
-  {/each}
+  <input bind:checked={item.status} type="checkbox" />
+  <span class:checked={item.status}>{item.task}</span>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <span on:click={() => deleteTodo(index)}>❌</span>
+{/each}
 </div>
 
 
 
-
-<!-- // {#each todoList as item, index}
-//   <input bind:checked={item.status} type="checkbox" />
-//   <span class:checked={item.status}>{item.text}</span>
-//   <!-- svelte-ignore a11y-click-events-have-key-events -->
-//   <!-- svelte-ignore a11y-no-static-element-interactions -->
-<!-- //   <span on:click={() => removeFromList(index)}>❌</span>
-//   <br />
-<style> --> -->
+<style>
   :root {
     --color-one: #f97316;
     --font-size: 1rem;
@@ -68,9 +63,15 @@
 
   .todo-list-container {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     margin-top: 2rem;
     gap: 5px;
+    width: 30rem;
+  }
+
+  .checked {
+    text-decoration: line-through;
   }
   /* 
   input {
