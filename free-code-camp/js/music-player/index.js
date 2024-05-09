ee0e70df02cead9c9a18d75020323ffa817040eb
elidvenega <robotsmailNinja@gmail.com>
2024-05-09 06:54:18 -0700

    Updated

--- free-code-camp/js/music-player/index.js
+++ free-code-camp/js/music-player/index.js
@@ -1,9 +1,11 @@
+// Getting all ELements with ID's
 const playlistSongs = document.getElementById("playlist-songs");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
 const nextButton = document.getElementById("next");
 const previousButton = document.getElementById("previous");
 const shuffleButton = document.getElementById("shuffle");
+
 
 const allSongs = [
   {
@@ -78,7 +80,10 @@ const allSongs = [
   },
 ];
 
+// using the Audio API
 const audio = new Audio();
+
+// making a copy of the all songs object
 let userData = {
   songs: [...allSongs],
   currentSong: null,
@@ -86,6 +91,7 @@ let userData = {
 };
 
 const playSong = (id) => {
+  // finds the current song
   const song = userData?.songs.find((song) => song.id === id);
   audio.src = song.src;
   audio.title = song.title;
