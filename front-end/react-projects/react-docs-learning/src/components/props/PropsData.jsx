2bf50dcb5c5f8486749abe98282da3a5dc2460e1
elidvenega <robotsmailNinja@gmail.com>
2024-05-11 23:48:40 -0700

    Updated

---
+++ front-end/react-projects/react-docs-learning/src/components/props/PropsData.jsx
@@ -0,0 +1,10 @@
+export default function PropsData({ name, language, country, age }) {
+  return (
+    <div>
+      <p>
+        Hello my {name} is and my age is {age} my language is{language} and
+        where Im from is {country}
+      </p>
+    </div>
+  );
+}
---
+++ front-end/react-projects/react-docs-learning/src/components/props/PropsPractice.jsx
@@ -0,0 +1,16 @@
+import PropsData from "./PropsData"
+
+
+
+function PropsPractice() {
+  return (
+    <div>
+     <PropsData 
+     name="Jack"
+     language="Spanish"
+     />
+    </div>
+  )
+}
+
+export default PropsPractice
