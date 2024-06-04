656286b529639a2a40260d0a093553c1aff8ab40
elidvenega <robotsmailNinja@gmail.com>
2024-06-03 22:31:47 -0700

    Updated

--- free-code-camp/js/calorie-counter/js/index.js
+++ free-code-camp/js/calorie-counter/js/index.js
@@ -1,3 +1,4 @@
+// variables
 const calorieCounter = document.getElementById("calorie-counter");
 const budgetNumberInput = document.getElementById("budget");
 const entryDropdown = document.getElementById("entry-dropdown");
@@ -6,6 +7,7 @@ const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
 let isError = false;
 
+// functions with regex
 function cleanInputString(str) {
   const regex = /[+-\s]/g;
   return str.replace(regex, "");
