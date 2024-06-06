eab19d2ef6c130cd51d117167c44a7e56d348ef2
elidvenega <robotsmailNinja@gmail.com>
2024-06-05 23:45:52 -0700

    Updated

--- front-end/react-projects/react-docs-learning/src/components/events/Button.jsx
+++ front-end/react-projects/react-docs-learning/src/components/events/Button.jsx
@@ -4,15 +4,13 @@ import "./buttonstyle.css";
 export default function Button() {
   const [btn, setBtn] = useState(0);
 
-  const handleIncrement = () => {
-    setBtn((btn) => btn + 1);
-  };
-  const handleDecrement = () => {
-    setBtn((btn) => btn - 1);
-  };
-  const resetButton = () => {
-    setBtn(0);
-  };
+  const handleIncrement = () => setBtn((btn) => btn + 1);
+
+  const handleDecrement = () => setBtn((btn) => btn - 1);
+
+  const resetButton = () => setBtn(0);
+
+  const alertButton = () => setBtn(alert("Hello"));
 
   return (
     <>
@@ -21,6 +19,7 @@ export default function Button() {
         <button onClick={handleIncrement}>Add</button>
         <button onClick={handleDecrement}>Subtract</button>
         <button onClick={resetButton}>Reset</button>
+        <button onClick={alertButton}>Message</button>
       </section>
     </>
   );
