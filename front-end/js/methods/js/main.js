252b213d19fe0943165452592d97800e2870d61c
elidvenega <robotsmailNinja@gmail.com>
2024-06-07 18:20:10 -0700

    Worked on learning methods

--- front-end/js/methods/js/main.js
+++ front-end/js/methods/js/main.js
@@ -2,6 +2,17 @@
 
 const str = `Hello world how are you and what are you doing`;
 
-str.split('');
+str.split("");
 
-console.log(str.split(''));
+console.log(str.split(""));
+
+// Reduce Method Practice
+
+const arr = [1, 2, 3, 4, 5, 60];
+
+const sum = arr.reduce((total, item) => total + item, 0);
+
+console.log(sum);
+
+
+// .selector > *
