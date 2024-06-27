ddb80618e23904edb0d57b4bf8ef8fd36828221c
elidvenega <robotsmailNinja@gmail.com>
2024-06-27 13:32:04 -0700

    Updated

--- front-end/typescript/typescript-practice/src/example.ts
+++ front-end/typescript/typescript-practice/src/example.ts
@@ -3,3 +3,18 @@ const msg: any = document.querySelector("#ts");
 const number: string = "Hello This is TS";
 
 msg.innerHTML = number;
+
+// Practice
+
+function ex(num: number) {
+  return num + num;
+}
+
+console.log(ex(3));
+
+// Generics
+function createPair<S, T>(v1: S, v2: T): [S, T] {
+  return [v1, v2];
+}
+
+console.log(createPair<string, number>("hello", 42)); // ['hello', 42]
