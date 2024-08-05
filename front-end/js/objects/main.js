e24178aed0f179314d385b9ff0a88c8320577909
elidvenega <robotsmailNinja@gmail.com>
2024-08-04 18:57:58 -0700

    Worked on JS methods and pratice JS fundamentals

--- front-end/js/objects/main.js
+++ front-end/js/objects/main.js
@@ -1,3 +1,5 @@
+const output = document.getElementById("output");
+const container = document.getElementById("container");
 // const obj = {
 //   arr: ["hello", "hi", "Today"],
 //   arr2: ["great", "wolfs", "t-rex"],
@@ -19,7 +21,35 @@ const food = {
   meal: "fish and veggies",
 };
 
-let breakfast = food['meal']
+let breakfast = food["meal"];
 
 console.log(breakfast);
 //console.log('hello');
+
+const exampleObj = {
+  name: "Jack",
+  lastName: "Harry",
+  occupation: "Mechanic",
+};
+
+output.textContent = `${exampleObj.name} ${exampleObj.lastName} is a ${exampleObj.occupation}`;
+//output.textContent = Object.output(exampleObj)
+
+// Example Chat GPT code how to display array object data to the dom
+
+const data = [
+  { id: 1, name: "Alice", age: 25 },
+  { id: 2, name: "Bob", age: 30 },
+  { id: 3, name: "Charlie", age: 35 },
+];
+
+const htmlElements = data.map((person) => {
+  return `<div>
+              <h2>${person.name}</h2>
+              <p>Age: ${person.age} and the id is ${person.id}</p>
+              
+          </div>`;
+});
+
+container.innerHTML = htmlElements.join("");
+// htmlElements.classList.add("container")
