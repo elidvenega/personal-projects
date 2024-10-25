e38ae963a29fa9902ead84f669f82312a4bfb8b3
elidvenega <robotsmailNinja@gmail.com>
2024-10-24 20:14:26 -0700

    Worked with svelte 5

---
+++ front-end/svelte-projects/svelte5/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Card from "./components/cardprojects/Card.svelte";
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<Card />
