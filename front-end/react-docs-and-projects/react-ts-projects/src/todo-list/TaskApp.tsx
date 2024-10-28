abe188e2365ed1b16e89e07cc96fefb1b595923c
elidvenega <robotsmailNinja@gmail.com>
2024-10-27 22:24:48 -0700

    Updated

--- front-end/react-docs-and-projects/react-ts-projects/src/todo-list/TaskApp.tsx
+++ front-end/react-docs-and-projects/react-ts-projects/src/todo-list/TaskApp.tsx
@@ -1,6 +1,8 @@
 import AddTask from "./AddTask";
 import TaskContext from "./TaskContext";
 import TaskList from "./TaskList";
+import "./todolist.css";
+
 
 export const TaskApp: React.FC = () => {
   return (
