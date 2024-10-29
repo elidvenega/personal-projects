aaefe7a642ca1cb77b55933e9c67660da05bee6d
elidvenega <robotsmailNinja@gmail.com>
2024-10-28 17:20:00 -0700

    Updated

--- front-end/react-docs-and-projects/react-docs-learning/src/components/events/Button.jsx
+++ front-end/react-docs-and-projects/react-docs-learning/src/components/events/Button.jsx
@@ -6,13 +6,22 @@ export default function Button() {
 
   const handleIncrement = () => setBtn((btn) => btn + 1);
 
+  // const handleDecrement = () => {
+  //   setBtn((btn) => Math.max(btn - 1, 0));
+  // };
+
   const handleDecrement = () => {
-    setBtn((btn) => Math.max(btn - 1, 0));
+    setBtn((btn) => {
+      if (btn >= 1) {
+        return btn - 1;
+      }
+      return 0;
+    });
   };
 
   const handleReset = () => setBtn(0);
 
-  const handleAlert = () => setBtn(alert(btn));
+  // const handleAlert = () => setBtn(alert(btn));
 
   const handleMultiplyByTwo = () => setBtn((btn) => btn * 2);
 
@@ -23,7 +32,7 @@ export default function Button() {
         <button onClick={handleIncrement}>Add</button>
         <button onClick={handleDecrement}>Subtract</button>
         <button onClick={handleReset}>Reset</button>
-        <button onClick={handleAlert}>Message</button>
+        {/* <button onClick={handleAlert}>Message</button> */}
         <button onClick={handleMultiplyByTwo}>Multiply By Two</button>
       </section>
     </>
