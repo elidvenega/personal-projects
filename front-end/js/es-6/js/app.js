09b41fde70a6285d8f40315851b52b09d3a9f3c8
elidvenega <robotsmailNinja@gmail.com>
2024-11-19 17:58:58 -0800

    Updated

---
+++ front-end/js/es-6/js/app.js
@@ -0,0 +1,17 @@
+"use strict";
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greet() {
+    console.log(`Hello, my name is ${this.name} and my age is ${this.age}`);
+  }
+}
+
+const person1 = new Person("Alice", 23);
+const person2 = new Person("Bob", 20);
+
+console.log(person2);
