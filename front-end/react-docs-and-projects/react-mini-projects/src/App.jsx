41a40fa9bf5509a833dfa326864ace7a43799080
elidvenega <venegaelid@gmail.com>
2024-12-22 21:28:40 -0800

    Worked with React

--- front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
@@ -1,11 +1,14 @@
-import RandomQuote from "./projects/random-quotes-server/RandomQuote";
+// import RandomQuote from "./projects/random-quotes-server/RandomQuote";
 // import IfPractice from "./projects/ifstatement/IfPractice";
+
+import API from "./projects/api/API";
 
 export default function App() {
   return (
     <>
-      <RandomQuote />
+      {/* <RandomQuote /> */}
       {/* <IfPractice /> */}
+      <API />
     </>
   );
 }
