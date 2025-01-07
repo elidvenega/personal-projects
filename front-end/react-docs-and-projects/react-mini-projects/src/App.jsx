ce9ae0f862cf0db0338718ac66a788bf5617f662
elidvenega <venegaelid@gmail.com>
2025-01-06 21:20:10 -0800

    Worked with React

--- front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
@@ -1,6 +1,8 @@
 // import RandomQuote from "./projects/random-quotes-server/RandomQuote";
 //  import IfPractice from "./projects/ifstatement/IfPractice";
 
+//import ProfileCard from "./frontend-mentrors/profile-card/ProfileCard";
+
 // import API from "./projects/api/API";
 import LocalMutation from "./projects/local-mutation/LocalMutation";
 
@@ -11,6 +13,7 @@ export default function App() {
       {/* <IfPractice /> */}
       {/* <API /> */}
       <LocalMutation />
+      {/* <ProfileCard /> */}
     </>
   );
 }
