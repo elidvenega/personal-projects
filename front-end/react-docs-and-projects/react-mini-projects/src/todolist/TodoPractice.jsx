c5259967884474471cc620fc3b32ec46b217a5aa
elidvenega <robotsmailNinja@gmail.com>
2025-01-18 21:20:13 -0800

    Updated

--- front-end/react-docs-and-projects/react-mini-projects/src/todolist/TodoPractice.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/todolist/TodoPractice.jsx
@@ -1,24 +1,55 @@
 import { useState } from "react";
 
-
-
-const todoList = ['Walk 20 mins', 'Eat Dinner', 'Read Book', 'Create Schedule for meeting'];
+const todoList = [
+  "Walk 20 mins",
+  "Eat Dinner",
+  "Read Book",
+  "Create Schedule for meeting",
+];
 
 export default function TodoPractice() {
-    const [todoList, setTodos] = useState(todoList);
-    const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(todoList);
+  const [input, setInput] = useState("");
+
+  const handleInput = (e) => setInput(e.target.value);
+
+  const handleSubmit = () => {
+    if (input.trim() !== "") {
+      setTodos([...todos, input]);
+      setInput("");
+    }
+  };
+
+  const handleDelete = (index) => {
+    const newList = [...todos];
+    newList.splice(index, 1);
+    setTodos(newList);
+  };
 
   return (
-  <>
-  <ul>
-    {
-        todoList.map(() => (
-            <li>
-
-            </li>
-        ))
-    }
-  </ul>
-  </>
-  )
+    <>
+      <h1>Todo List</h1>
+      <form action="">
+        <input
+          type="text"
+          value={input}
+          placeholder="Add Todo"
+          onChange={handleInput}
+        />
+        <button type="button" onClick={handleSubmit}>
+          Add
+        </button>
+      </form>
+      <ul>
+        {todos.map((todo, i) => (
+          <li key={i}>
+            {todo}
+            <button type="button" onClick={() => handleDelete(i)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
