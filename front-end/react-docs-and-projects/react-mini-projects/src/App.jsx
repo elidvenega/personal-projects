f9de98024ef553ec588050949d04ea60b69dcefc
elidvenega <venegaelid@gmail.com>
2025-03-12 12:54:34 -0700

    Worked on React

--- front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
@@ -1,12 +1,19 @@
 // import AccordionPractice from "./projects/accordion/AccordionPractice";
-import SecondAccordion from "./projects/accordion/SecondAccordion";
+// import SecondAccordion from "./projects/accordion/SecondAccordion";
+
+import FourthAccordion from "./projects/accordion/FourthAccordion";
+
+// import Accordion from "./projects/accordion-practice/Accordion";
+// import ThirdAccordion from "./projects/accordion/ThirdAccordion";
 
 export default function App() {
   return (
     <>
       {/* <AccordionPractice /> */}
-      <SecondAccordion />
+      {/* <SecondAccordion /> */}
+      {/* <ThirdAccordion /> */}
+      {/* <Accordion /> */}
+      <FourthAccordion />
     </>
-  
-  )
+  );
 }
