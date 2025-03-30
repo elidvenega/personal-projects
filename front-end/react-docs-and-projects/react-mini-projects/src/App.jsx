d73473b2dfb648350408df79cb8390e50cdfdacb
elidvenega <robotsmailNinja@gmail.com>
2025-03-29 23:30:30 -0700

    Updated

--- front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
@@ -1,34 +1,9 @@
-// import AccordionPractice from "./projects/accordion/AccordionPractice";
-// import SecondAccordion from "./projects/accordion/SecondAccordion";
-
-// import EightAccordion from "./projects/accordion/EightAccordion";
-import ThirdPracticeTodo from "./projects/todolist/ThirdPracticeTodo";
-
-// import Springroll from "./animations/Springroll";
-// import SeventhAccordion from "./projects/accordion/SeventhAccordion";
-
-// import FifthAccordion from "./projects/accordion/FifthAccordion";
-// import SixthAccordion from "./projects/accordion/SixthAccordion";
-
-// import FourthAccordion from "./projects/accordion/FourthAccordion";
-
-// import Accordion from "./projects/accordion-practice/Accordion";
-// import ThirdAccordion from "./projects/accordion/ThirdAccordion";
+import NineAccordion from "./projects/accordion/NineAccordion";
 
 export default function App() {
   return (
     <>
-      {/* <AccordionPractice /> */}
-      {/* <SecondAccordion /> */}
-      {/* <ThirdAccordion /> */}
-      {/* <Accordion /> */}
-      {/* <FourthAccordion /> */}
-      {/* <FifthAccordion /> */}
-      {/* <SixthAccordion /> */}
-      {/* <SeventhAccordion /> */}
-      {/* <EightAccordion /> */}
-      <ThirdPracticeTodo  title={"Todo List"}/>
-      {/* <Springroll /> */}
+   <NineAccordion />
     </>
   );
 }
