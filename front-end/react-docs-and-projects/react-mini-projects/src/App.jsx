e36320e0b0db04e2ccb823dc1b98b4fcf29bcaad
elidvenega <robotsmailNinja@gmail.com>
2025-05-05 15:28:41 -0700

    Updated

--- front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
+
 // import NineAccordion from "./projects/accordion/NineAccordion";
-import DarkTheme from "./projects/dark-theme/DarkTheme";
+// import DarkTheme from "./projects/dark-theme/DarkTheme";
 // import Destructuring from "./projects/destructuring/Destructuring";
 
 export default function App() {
+  const [bgColor, setBGColor] = useState(false);
+
+  const handleBGColor = () => {
+    setBGColor(!bgColor);
+  };
   return (
-    <>
+    <div className={`${bgColor ? `dark`: `light`}`}>
       {/* <Destructuring /> */}
-      <DarkTheme />
-    </>
+      {/* <DarkTheme /> */}
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+        optio nihil minus minima ratione eum quaerat corrupti, eveniet officia
+        aliquam natus quis deserunt, impedit sint dicta vero consectetur facilis
+        nisi!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+        optio nihil minus minima ratione eum quaerat corrupti, eveniet officia
+        aliquam natus quis deserunt, impedit sint dicta vero consectetur facilis
+        nisi!
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae
+        optio nihil minus minima ratione eum quaerat corrupti, eveniet officia
+        aliquam natus quis deserunt, impedit sint dicta vero consectetur facilis
+        nisi!
+      </p>
+      <button onClick={handleBGColor}>click</button>
+    </div>
   );
 }
