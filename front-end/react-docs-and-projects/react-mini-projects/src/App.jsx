3076e278818153de367160f1f18e21bdb1be520c
elidvenega <robotsmailNinja@gmail.com>
2025-05-26 19:12:49 -0700

    "Worked on a gallery project"

--- front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
@@ -1,9 +1,11 @@
 import GalleryPractice from "./projects/image-slide-project/GalleryPractice";
+import PracticeGalleryOne from "./projects/image-slide-project/PracticeGalleryOne";
 
 export default function App() {
   return (
     <>
       <GalleryPractice />
+      <PracticeGalleryOne />
     </>
   );
 }
