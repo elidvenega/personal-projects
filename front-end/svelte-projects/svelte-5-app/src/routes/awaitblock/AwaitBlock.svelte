136a04450119bffb68fa4b09f7c7a4d0a2513dad
elidvenega <robotsmailNinja@gmail.com>
2025-06-14 21:39:38 -0700

    Worked on Svelte and gerring more familar with this framework

---
+++ front-end/svelte-projects/svelte-5-app/src/routes/awaitblock/AwaitBlock.svelte
@@ -0,0 +1,21 @@
+<script>
+  let userPromise = fetch('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => {
+      if (!res.ok) throw new Error('Network error');
+      return res.json();
+    });
+</script>
+
+<h2>User Profile</h2>
+
+{#await userPromise}
+  <p>Loading user...</p>
+{:then user}
+  <div>
+    <p><strong>Name:</strong> {user.name}</p>
+    <p><strong>Email:</strong> {user.email}</p>
+    <p><strong>Website:</strong> {user.website}</p>
+  </div>
+{:catch error}
+  <p style="color: red;">Error loading user: {error.message}</p>
+{/await}
