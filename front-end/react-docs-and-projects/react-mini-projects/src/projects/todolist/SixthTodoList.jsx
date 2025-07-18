469ec5e80bfff8e3dea057ba2d447c9094462f95
elidvenega <robotsmailNinja@gmail.com>
2025-07-17 23:51:08 -0700

    Worked on React

--- front-end/react-docs-and-projects/react-mini-projects/src/projects/todolist/SixthTodoList.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/projects/todolist/SixthTodoList.jsx
@@ -1,27 +1,53 @@
-import { useState } from 'react';
-import { data } from './data';
+import { useState } from "react";
+import { data } from "./data";
 
+let nextTodo = 5;
 export default function SixthTodoList() {
-    const [input, setInput] = useState("");
-    const [todolist, setTodoList] = useState(data)
+  const [input, setInput] = useState("");
+  const [todolist, setTodoList] = useState(data);
 
-    
+  const handleAddTodo = () => {
+    if (input.trim() === "") return;
+    const newTodos = {
+      id: nextTodo++,
+      task: input,
+    };
+
+    setTodoList([...todolist, newTodos]);
+    setInput("");
+  };
+
+  const handleDelete = (id) => {
+    const newList = todolist.filter((todo) => todo.id !== id);
+    setTodoList(newList);
+  };
+
   return (
     <div>
-        <form action="">
-            <input 
-            type="text"
-            value={input} 
-            
-            />
-            <button>Add</button>
-        </form>
-
-        <ul>
-            {
-
-            }
-        </ul>
+      <form action="">
+        <h1>Todo List</h1>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => {
+            e.preventDefault();
+            setInput(e.target.value);
+          }}
+          placeholder="Add todo"
+        />
+        <button type="button" onClick={handleAddTodo}>
+          Add
+        </button>
+      </form>
+      <ul>
+        {todolist.map((todo) => (
+          <li key={todo.id}>
+            {todo.task}
+            <button onClick={() => handleDelete(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      div+p_ul
     </div>
-  )
+  );
 }
