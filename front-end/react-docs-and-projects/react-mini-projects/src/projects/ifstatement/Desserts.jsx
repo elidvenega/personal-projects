6bd90075cbd169fb4174e945bafa4a6137a93a9b
elidvenega <robotsmailNinja@gmail.com>
2025-09-28 01:06:50 -0700

    Updated

--- front-end/react-docs-and-projects/react-mini-projects/src/projects/ifstatement/Desserts.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/projects/ifstatement/Desserts.jsx
@@ -1,41 +1,21 @@
 import { useState } from "react";
 import "./styles.css";
-// const sectionStyles = {
-//   backgroundColor: "#1f2937",
-//   fontSize: "1.5rem",
-//   color: "#fff",
-//   padding: "3.5rem",
-//   margin: "auto",
-//   textAlign: "center",
-// };
-
-// const buttonStyles = {
-//   backgroundColor: "#a855f7",
-//   border: "none",
-//   borderRadius: "5px",
-//   color: "#fff",
-//   padding: "8px 12px",
-//   textAlign: "center",
-//   margin: "5px",
-//   cursor: "pointer",
-//   fontSize: "1rem",
-// };
 
 // Dessert data
 const desserts = [
   {
     name: "Pumpkin Pie",
-    image: "../../../public/images/pumpkin-pie.jpg",
+    image: "../../../images/pumpkin-pie.jpg",
     message: "Enjoy the Pumpkin Pie!",
   },
   {
     name: "Apple Pie",
-    image: "../../../public/images/apple-pie.jpg",
+    image: "../../../images/apple-pie.jpg",
     message: "Sweet and warm Apple Pie!",
   },
   {
     name: "Chocolate Cake",
-    image: "../../../public/images/choclate-cake.jpg",
+    image: "../../../images/choclate-cake.jpg",
     message: "Delicious Chocolate Cake!",
   },
 ];
@@ -43,8 +23,15 @@ const desserts = [
 export default function Desserts() {
   const [index, setIndex] = useState(0);
 
+  // No entiendo porque se esta usando modulo
   const handleNextDessert = () => {
     setIndex((prevIndex) => (prevIndex + 1) % desserts.length);
+  };
+
+  const handlePrevDessert = () => {
+    setIndex(
+      (prevIndex) => (prevIndex - 1 + desserts.length) % desserts.length
+    );
   };
 
   const dessert = desserts[index];
@@ -53,17 +40,7 @@ export default function Desserts() {
     <>
       <section className="sectionStyles">
         <h1>{dessert.name}</h1>
-        <img
-          style={{
-            borderRadius: "10%",
-            width: "22rem",
-            height: "22rem",
-            objectFit: "cover",
-            padding: "1rem",
-          }}
-          src={dessert.image}
-          alt={dessert.name}
-        />
+        <img className="image" src={dessert.image} alt={dessert.name} />
         <p>{dessert.message}</p>
 
         <button
@@ -72,6 +49,13 @@ export default function Desserts() {
           onClick={handleNextDessert}
         >
           Next Dessert
+        </button>
+        <button
+          className="buttonStyles"
+          type="button"
+          onClick={handlePrevDessert}
+        >
+          Last Dessert
         </button>
       </section>
     </>
