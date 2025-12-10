c97e0dd366921bc4efbd7da25c1567be65853ef4
elidvenega <robotsmailNinja@gmail.com>
2025-12-10 11:37:21 -0800

    Worked on JS and the DOM

--- front-end/js/projects/filterlist/js/app.js
+++ front-end/js/projects/filterlist/js/app.js
@@ -1,15 +1,30 @@
-const searchItems = document.querySelector(".fruitSearch"); 
+const searchInput = document.querySelector(".searchInput");
 const listItems = document.querySelectorAll(".listItems li");
 
+// Filters the list items based on the user's input in the search box by showing or hiding items that match the filter.
+function filterListItems() {
+  const filter = searchInput.value.toLowerCase();
 
-function filterList() {
-  const filter = searchItems.value.toLowerCase();
-  searchItems.forEach((item) => {
-    item.style.display = listItems.toLowerCase().includes(filter)
+  listItems.forEach((item) => {
+    item.style.display = item.textContent.toLowerCase().includes(filter)
       ? "block"
       : "none";
   });
 }
 
-searchItems.addEventListener("keyup", filterList);
+searchInput.addEventListener("keyup", filterListItems);
 
+// Filters the list items based on the user's input in the search box by showing or hiding items that match the filter.
+// function filterListItems() {
+//   const filter = searchInput.value.toLowerCase();
+
+//   listItems.forEach((item) => {
+//     if (item.textContent.toLowerCase().includes(filter)) {
+//       item.style.display = "block";
+//     } else {
+//       item.style.display = "none";
+//     }
+//   });
+// }
+
+searchInput.addEventListener("keyup", filterListItems);
