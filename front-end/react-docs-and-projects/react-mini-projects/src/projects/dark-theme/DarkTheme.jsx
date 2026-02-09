a27bd452e9e3b6f17ec3eb17360ceb68a6fad46c
elidvenega <robotsmailNinja@gmail.com>
2026-02-08 23:56:53 -0800

    updated

--- front-end/react-docs-and-projects/react-mini-projects/src/projects/dark-theme/DarkTheme.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/projects/dark-theme/DarkTheme.jsx
@@ -1,45 +1,45 @@
-import { createContext, useContext, useState } from "react";
+// import { createContext, useContext, useState } from "react";
 
-// Create a context with a default value
-const ThemeContext = createContext();
+// // Create a context with a default value
+// const ThemeContext = createContext();
 
-export default function DarkTheme() {
-  const [theme, setTheme] = useState("light"); // Light or dark theme
+// export default function DarkTheme() {
+//   const [theme, setTheme] = useState("light"); // Light or dark theme
 
-  return (
-    <ThemeContext.Provider value={{ theme, setTheme }}>
-      <Toolbar />
-    </ThemeContext.Provider>
-  );
-}
+//   return (
+//     <ThemeContext.Provider value={{ theme, setTheme }}>
+//       <Toolbar />
+//     </ThemeContext.Provider>
+//   );
+// }
 
-function Toolbar() {
-  return (
-    <div>
-      <ThemedButton />
-    </div>
-  );
-}
+// function Toolbar() {
+//   return (
+//     <div>
+//       <ThemedButton />
+//     </div>
+//   );
+// }
 
-function ThemedButton() {
-  const { theme, setTheme } = useContext(ThemeContext); // Access the context value
+// function ThemedButton() {
+//   const { theme, setTheme } = useContext(ThemeContext); // Access the context value
 
-  return (
-    <div>
-      <p
-        style={{
-          background: theme === "light" ? "#fff" : "#333",
-          color: theme === "light" ? "#000" : "#fff",
-        }}
-      >
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet,
-        deserunt totam quasi iusto, possimus nesciunt corporis, natus facere
-        eius vitae voluptatum voluptates neque officiis eum accusamus quia
-        libero nulla autem!
-      </p>
-      <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-        Toggle Theme
-      </button>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <p
+//         style={{
+//           background: theme === "light" ? "#fff" : "#333",
+//           color: theme === "light" ? "#000" : "#fff",
+//         }}
+//       >
+//         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet,
+//         deserunt totam quasi iusto, possimus nesciunt corporis, natus facere
+//         eius vitae voluptatum voluptates neque officiis eum accusamus quia
+//         libero nulla autem!
+//       </p>
+//       <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+//         Toggle Theme
+//       </button>
+//     </div>
+//   );
+// }
