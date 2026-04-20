7009bae1fb9cd0eacd4e309a8e8d1f36a3d3f3f2
elidvenega <robotsmailNinja@gmail.com>
2026-04-19 22:17:36 -0700

    Updated

--- front-end/react-docs-and-projects/reducecontext-todolist/src/practice-todos/practice-todo/TaskList.jsx
+++ front-end/react-docs-and-projects/reducecontext-todolist/src/practice-todos/practice-todo/TaskList.jsx
@@ -2,18 +2,18 @@
 import { useState } from "react";
 import { useTasks, useDispatch } from "./ContextFunction";
 
-export default function TaskList() {
-  const tasks = useTasks();
-  return (
-    <ul>
-      {tasks.map((task) => (
-        <li key={task.id}>
-          <Task task={task} />
-        </li>
-      ))}
-    </ul>
-  );
-}
+// export default function TaskList() {
+//   const tasks = useTasks();
+//   return (
+//     <ul>
+//       {tasks.map((task) => (
+//         <li key={task.id}>
+//           <Task task={task} />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// }
 
 function Task({ task }) {
   const [isEditing, setIsEditing] = useState(false);
