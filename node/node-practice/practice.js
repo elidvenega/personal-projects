01f75bd7be6cb014ca67a2d70a0fd3c741d3ae83
elidvenega <robotsmailNinja@gmail.com>
2026-05-01 14:21:52 -0700

    Worked with inserting text into a blank .txt

--- node/node-practice/practice.js
+++ node/node-practice/practice.js
@@ -1,8 +1,10 @@
 import { writeFile } from "node:fs";
 import { Buffer } from "node:buffer";
 
+const shortMsg = `be great and dont give up`;
+
 const message = `
-Hello I hope you are good
+Hello I hope you are good and this is a new message that I'am writing ${shortMsg}
 
 `;
 
