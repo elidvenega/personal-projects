c9f05db07bea50d9fc6b23a7b6b309a14868fb1b
elidvenega <robotsmailNinja@gmail.com>
2026-05-28 14:55:37 -0700

    Updated

---
+++ courses/free-code-camp/js/emoji-reactor/js/apptwo.js
@@ -0,0 +1,32 @@
+// final code
+
+// function updateCount(btn) {
+//   const countEl = btn.querySelector(".count");
+//   let currCount = +countEl.textContent.split("/")[0];
+
+//   if (currCount === 10) return;
+
+//   currCount++;
+
+//   countEl.textContent = `${currCount}/10`;
+// }
+
+// const btns = document.querySelectorAll(".emoji-btn");
+
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", () => updateCount(btn));
+// });
+
+// #1 first one writing 
+function updateCount(btn) {
+    const countEl = btn.querySelector(".count");
+    let currCount = +countEl.textContent.split("/")[0];
+
+
+    if (currCount === 10) return;
+
+    currCount++;
+
+    countEl.textContent = `${currCount}/10`;
+
+}
