a65a877889305b643c39fdd6f4d34177dd29e7b1
elidvenega <robotsmailNinja@gmail.com>
2026-06-29 01:32:02 -0700

    Updated

--- front-end/react-docs-and-projects/react-mini-projects/src/fcc-musicfilter/MusicFilterTwo.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/fcc-musicfilter/MusicFilterTwo.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react';
-import { instrumentsArr } from './instruments';
+import { useState } from "react";
+import { instrumentsArr } from "./instruments";
 
 export default function MusicFilterTwo() {
-    const [category, setCategory] = useState("all");
-  
-    const filteredList
+  const [category, setCategory] = useState("all");
+
+  const instrumentFilter =
+    category === "all"
+      ? instrumentsArr
+      : instrumentsArr.filter((instrument) => instrument.category === category);
+
+  const handleInput = (e) => setCategory(e.target.value);
 
   return (
-     <main>
-        <select value={category} onChange={(e) => setCategory(e.target.value)}>
-            <option value="all">All</option>
-            <option value="woodwinds">Woodwinds</option>
-            <option value="brass">Brass</option>
-            <option value="percussion">Percussion</option>
-        </select>
-     </main>
-  )
+    <main>
+      <h1>Todo list</h1>
+      <select value={category} onChange={handleInput}>
+        <option value="all">All</option>
+        <option value="woodwinds">Woodwinds</option>
+        <option value="brass">Brass</option>
+        <option value="percussion">Percussion</option>
+      </select>
+
+      {instrumentFilter.map((item) => (
+        <div>
+          <h2>{item.instrument}</h2>
+          <p>{item.price}</p>
+        </div>
+      ))}
+    </main>
+  );
 }
