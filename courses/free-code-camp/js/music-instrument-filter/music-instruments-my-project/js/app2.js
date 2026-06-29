230dacd493950dfefa41882968fb6a9f26922674
elidvenega <robotsmailNinja@gmail.com>
2026-06-29 14:57:49 -0700

    Worked on DOM projects and React projects

---
+++ courses/free-code-camp/js/music-instrument-filter/music-instruments-my-project/js/app2.js
@@ -0,0 +1,40 @@
+const instrumentsArr = [
+  { category: "woodwinds", instrument: "Flute", price: 500 },
+  { category: "woodwinds", instrument: "Clarinet", price: 200 },
+  { category: "woodwinds", instrument: "Oboe", price: 4000 },
+  { category: "brass", instrument: "Trumpet", price: 200 },
+  { category: "brass", instrument: "Trombone", price: 300 },
+  { category: "brass", instrument: "French Horn", price: 4300 },
+  { category: "percussion", instrument: "Drum Set", price: 500 },
+  { category: "percussion", instrument: "Xylophone", price: 3000 },
+  { category: "percussion", instrument: "Cymbals", price: 200 },
+  { category: "percussion", instrument: "Marimba", price: 3000 },
+];
+
+const selectContainer = document.querySelector("select");
+const productContainer = document.querySelector(".product-container");
+
+function instrumentCards(instrumentCategory) {
+  const instruments =
+    instrumentCategory === "all"
+      ? instrumentsArr
+      : instrumentsArr.filter(
+          ({ category }) => category === instrumentCategory,
+        );
+
+  return instruments.map(
+    ({ instrument, price }) =>
+      `
+        <div class="card">
+          <h2>${instrument}</h2>
+          <p>$${price}</p>
+        </div>
+       `,
+  );
+}
+
+selectContainer.addEventListener("change", () => {
+  productContainer.innerHTML = instrumentCards(selectContainer.value);
+});
+
+console.log("Hello");
