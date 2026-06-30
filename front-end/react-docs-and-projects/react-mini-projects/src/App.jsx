941c6003ec453d0a7445bed3fe49b2d48ac61ccf
elidvenega <robotsmailNinja@gmail.com>
2026-06-30 14:39:21 -0700

    Worked on DOM projects and React projects

--- front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/App.jsx
@@ -1,24 +1,9 @@
-// import DessertCarousel from "./projects/carousel-gpt/DessertCarousel";
-// import Counter from "./projects/useEffect-practice/counterEffect/Counter";
-// import UseEffectPractice from "./projects/useEffect-practice/projectone/UseEffectPractice";
-// import RickAndMorty from "./projects/rick-and-morty/RickAndMorty";
-// import MusicFilterThree from "./fcc-musicfilter/MusicFilterThree";
-// import MusicFilter from "./fcc-musicfilter/MusicFilter";
-
-import MusicFilterTwo from "./fcc-musicfilter/MusicFilterTwo";
-// import Counter from "./custom-hooks/Counter";
+import DessertPractice from "./projects/carousel/DessertPractice";
 
 export default function App() {
   return (
     <>
-      {/* <DessertCarousel /> */}
-      {/* <UseEffectPractice /> */}
-      {/* <Counter /> */}
-      {/* <RickAndMorty /> */}
-      {/* <Counter /> */}
-      {/* <MusicFilter /> */}
-      <MusicFilterTwo />
-      {/* <MusicFilterThree /> */}
+     <DessertPractice />
     </>
   );
 }
