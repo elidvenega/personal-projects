1168ee55789f3f44794040f0f5a7c28b4e9421f5
elidvenega <robotsmailNinja@gmail.com>
2026-07-22 14:10:02 -0700

    Created a carousel

--- front-end/react-docs-and-projects/react-mini-projects/src/projects/carousel/FourthDessertPractice.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/projects/carousel/FourthDessertPractice.jsx
@@ -1,7 +1,3 @@
-import React, { useState } from "react";
-import "./styles.css";
-import { desserts } from "./data";
-
 /* 
 CSS Clasess im using
 Section Tag Im using "sectionStyles"
@@ -10,18 +6,40 @@ Image tag has "image" class
 
 */
 
+import { useState } from "react";
+import { desserts } from "./data";
+import "./styles.css";
+
 export default function FourthDessertPractice() {
   const [dessertIndex, setDessertIndex] = useState(0);
 
-  // create a function to go to the next picture
-  const nextIndex = () =>
-    setDessertIndex((nextIndex) => (nextIndex + 1) % desserts.length);
+  const handleNextDessert = () =>
+    setDessertIndex((nextDessert) => (nextDessert + 1) % desserts.length);
+  const handlePrevDessert = () =>
+    setDessertIndex(
+      (prevDessert) => (prevDessert - 1 + desserts.length) % desserts.length,
+    );
 
   const dessert = desserts[dessertIndex];
   return (
     <section className="sectionStyles">
-      <img src={dessert.image} className="image" alt="dessert" />
-      <button onClick={nextIndex}>Next</button>
+      <h1>{dessert.name}</h1>
+      <img className="image" src={dessert.image} alt={dessert.alt} />
+      <p>{dessert.message}</p>
+      <button
+        className="buttonStyles"
+        type="button"
+        onClick={handleNextDessert}
+      >
+        Next
+      </button>
+      <button
+        className="buttonStyles"
+        type="button"
+        onClick={handlePrevDessert}
+      >
+        Prev
+      </button>
     </section>
   );
 }
--- front-end/react-docs-and-projects/react-mini-projects/src/projects/carousel/data.js
+++ front-end/react-docs-and-projects/react-mini-projects/src/projects/carousel/data.js
@@ -3,15 +3,18 @@ export const desserts = [
     name: "Pumpkin Pie",
     image: "../../../images/pumpkin-pie.jpg",
     message: "Enjoy the Pumpkin Pie!",
+    alt: "pumpkin pie"
   },
   {
     name: "Apple Pie",
     image: "../../../images/apple-pie.jpg",
     message: "Sweet and warm Apple Pie!",
+    alt: "apple pie"
   },
   {
     name: "Chocolate Cake",
     image: "../../../images/choclate-cake.jpg",
     message: "Delicious Chocolate Cake!",
+    alt: "choclate cake"
   },
 ];
