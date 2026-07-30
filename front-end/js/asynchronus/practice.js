40bca7e4ecb9afa0652e5657d73fd09b81daa235
elidvenega <robotsmailNinja@gmail.com>
2026-07-30 14:56:26 -0700

    Updated

--- front-end/js/asynchronus/practice.js
+++ front-end/js/asynchronus/practice.js
@@ -13,3 +13,25 @@ practicePromise
   .catch((failure) => {
     console.log(failure);
   });
+
+
+
+  let practicePromiseTwo = new Promise((resolve, reject) => {
+  if (1 === 1) {
+    resolve("Success");
+  } else {
+    reject("Failure");
+  }
+});
+
+practicePromise
+  .then((message) => {
+    console.log(message);
+  })
+  .catch((failure) => {
+    console.log(failure);
+  });
+
+
+  
+  
