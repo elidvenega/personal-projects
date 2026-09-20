acdaa43ac25d408b879092aa8a4960189d67d0b9
elidvenega <robotsmailNinja@gmail.com>
2026-09-20 14:14:26 -0700

    Updated

--- front-end/react-docs-and-projects/react-mini-projects/src/projects/todolist/FoodTodoList.jsx
+++ front-end/react-docs-and-projects/react-mini-projects/src/projects/todolist/FoodTodoList.jsx
@@ -1,24 +1,25 @@
-import { useState } from 'react';
-import {foods} from "./foodData.js";
-import { li } from 'framer-motion/client';
+import { useState } from "react";
+import { foods } from "./foodData";
 
 export default function FoodTodoList() {
-    const [input, setInput] = useState("");
-    const [todos, setTodos] = useState(foods);
+  const [input, setInput] = useState("");
+  const [foodList, setFoodList] = useState(foods);
 
   return (
-    <div>
-        <h1>Food List</h1>
-        <input type="text" />
-        <ul>
-            {
-              foods.map((food) => (
-                <li>
-                    
-                </li>
-              ))
-            }
-        </ul>
-    </div>
-  )
+    <>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={input}
+        placeholder="Add food"
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <ul>
+        {foodList.map((item) => (
+          <li key={item.id}>{item.food}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
